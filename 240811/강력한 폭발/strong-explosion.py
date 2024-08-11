102a769cc6043f11e0ef26043f765ffc59a824bc
Sasha <82912217+Yeeun-Lee-sudo@users.noreply.github.com>
2024-08-11 21:34:02 +0900

    Title: 강력한 폭발, Time: 5023ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240811/강력한 폭발/strong-explosion.py
+++ 240811/강력한 폭발/strong-explosion.py
@@ -1,5 +1,9 @@
 n = int(input())
-arr = []
+bombed_m = [ [False for i in range(n)] for j in range(n)]
+bomb_type = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
 bomblist = []
 
 for i in range(n):
@@ -8,30 +12,6 @@ for i in range(n):
         bomblist.append([ll.index(1), i])
 
 #print(bomblist)
-'''
-
-def bomb1(x, y):
-    arr.append([x, y])
-    arr.append([x, y-1])
-    arr.append([x, y-2])
-    arr.append([x, y+1])
-    arr.append([x, y+2])
-
-def bomb2(x, y):
-    arr.append([x, y])
-    arr.append([x, y-1])
-    arr.append([x-1, y])
-    arr.append([x, y+1])
-    arr.append([x+1, y])
-
-def bomb3(x, y):
-    arr.append([x, y])
-    arr.append([x-1, y-1])
-    arr.append([x-1, y+1])
-    arr.append([x+1, y+1])
-    arr.append([x+1, y-1])
-
-'''
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
@@ -39,43 +19,55 @@ def in_range(x, y):
 def bomb(x, y, b_type):
     # 폭탄 종류마다 터질 위치를 미리 정의합니다.
     bomb_shapes = [
+        [], 
         [[-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0]],
         [[-1, 0], [1, 0], [0, 0], [0, -1], [0, 1]],
         [[-1, -1], [-1, 1], [0, 0], [1, -1], [1, 1]]
     ]
-    num = 0
+
     # 격자 내 칸에 대해서만 영역을 표시합니다.
     for i in range(5):
         dx, dy = bomb_shapes[b_type][i];
         nx, ny = x + dx, y + dy
         if in_range(nx, ny):
-            if [nx, ny] not in arr:
-                arr.append([nx, ny])
-                num += 1
-    return num
+            bombed_m[nx][ny] = True
+
     #print(arr)
 
-max = 0
+ans = 0
+
+def count():
+    for i in range(n):
+        for j in range(n):
+            bombed_m[i][j] = False
+    
+    for i in range(n):
+        for j in range(n):
+            if bomb_type[i][j]:
+                bomb(i, j, bomb_type[i][j])
+
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if bombed_m[i][j]:
+                cnt += 1
+    
+    return cnt
 
 def search(bombs):
     #폭탄 놓을 위치를 저장해 둔 배열을 받음
-    global max
+    global ans
 
     if len(bombs) == 0:
-        a = len(arr)
-        if a >= max:
-            max = a
-            #print(max)
+        ans = max(ans, count())
         return
 
     for i in bombs:
         for j in range(3):
-            nn = bomb(i[1], i[0], j)
+            bomb_type[i[1]][i[0]] = j
             search(bombs[1:])
-            for p in range(nn):
-                arr.pop()
 
     return
 
 search(bomblist)
-print(max)
+print(ans)
