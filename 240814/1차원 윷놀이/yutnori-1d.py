1ba3cdf1913129da4fb539b3693a58c53938ee05
Sasha <82912217+Yeeun-Lee-sudo@users.noreply.github.com>
2024-08-14 22:21:17 +0900

    Title: 1차원 윷놀이, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240814/1차원 윷놀이/yutnori-1d.py
+++ 240814/1차원 윷놀이/yutnori-1d.py
@@ -1,26 +1,29 @@
 n, m, k = map(int, input().split())
 move = list(map(int, input().split()))
-grid = [i for i in range(1, m+1)]
-pos = [0 for j in range(k)]
+grid = [1 for i in range(m)]
+pos = [1 for j in range(k)]
 ans = 0
 sco = []
 
 def choose(cnt):
     global ans
     global sco
-    if cnt == len(move)-1:
-        ans = max(ans, len(sco))
+    if cnt == len(move):
+        #print(*pos)
+        #print(*grid)
+        ans = max(ans, sum(sco))
         return
     
     for j in range(k):
-        pos[j] += move[cnt]
-        if grid[pos[j]%m] == 0:
-            choose(cnt+1)
-            pos[j] -= move[cnt]
+        if pos[j] >= m:
             continue
-        else:
-            grid[pos[j]%m] = 0
+        
+        pos[j] += move[cnt]
+        if pos[j] == m:
             sco.append(1)
+            #print(*pos)
+        else:
+            sco.append(0)
         choose(cnt+1)
         pos[j] -= move[cnt]
         sco.pop()
