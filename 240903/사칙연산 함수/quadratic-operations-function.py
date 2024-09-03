e9f04ddd4eac3ef40ef18924df25576447fa7f2b
Sasha <82912217+Yeeun-Lee-sudo@users.noreply.github.com>
2024-09-03 18:11:19 +0900

    Title: 사칙연산 함수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240903/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,27 @@
+def plus(num1, num2):
+    return num1 + num2
+
+def minus(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+print("%d %c %d = " %(a, o, c), end="")
+if o == "+":
+    print(plus(a, c))
+elif o == "-":
+    print(minus(a, c))
+elif o == "*":
+    print(multiply(a, c))
+elif o == "/":
+    print(divide(a, c))
+else:
+    print("False")
